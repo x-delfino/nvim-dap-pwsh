20707cb18483ecb994b465d7eb2c8c69ba03352d
aled <aled.mehta@withsecure.com>
2023-04-28 19:13:28 +0100

    wip

---
+++ pwsh/Load-EditorServices.ps1
@@ -0,0 +1,10 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$BundlePath, 
+    [Parameter(Mandatory=$true)]
+    [string]$LogPath, 
+    [Parameter(Mandatory=$true)]
+    [string]$SessionDetailsPath
+)
+
+
